840ec2922d848bd60ad6a5e8348912c7c1eb11ff
ifejinity <mrlonzanida08@gmail.com>
2023-04-28 00:43:56 +0800

    v2

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const buttons = document.querySelectorAll("button");
 const btnCount = document.querySelector(".buttons").childElementCount;
-const sCreen = document.querySelector(".screen");
+const exPression = document.querySelector(".expression");
+const reSult = document.querySelector(".result");
+const siGn = document.querySelector(".signs");
+
 let myArray = [];
 let screenState = 0;
 let myFunction = "";
-sCreen.value = "";
+let expressionState = 0;
+exPression.innerHTML = "";
 toTal = 0;
 for (button of buttons){
     button.addEventListener("click", function(){
@@ -12,51 +16,91 @@ for (button of buttons){
             if(this == buttons[i]){
                 try{
                     if (this.innerHTML == "="){
-                        const expression = myArray.join("");
-                        toTal = eval(expression);
-                        myArray = [];
-                        myArray.push(toTal);
-                        sCreen.value = toTal;
-                        console.log(myArray);
+                        if (myArray.length == 0){
+
+                        }
+                        else{
+                            expressionState = 1;
+                            const expression = myArray.join("");
+                            toTal = eval(expression).toFixed(2);
+                            myArray = [];
+                            myArray.push(toTal);
+                            reSult.innerHTML = toTal;
+                            siGn.innerHTML = this.innerHTML;
+                            console.log(myArray);
+                        }
                     }
                     else if (this.innerHTML == "-" || this.innerHTML == "+" || this.innerHTML == "*" || this.innerHTML == "/" || this.innerHTML == "." ){
                         if(screenState == 1){
                             screenState = 0;
-                            sCreen.value = this.innerHTML;
+                            siGn.innerHTML = this.innerHTML;
+                            exPression.innerHTML = this.innerHTML;
+                            reSult.innerHTML = "";
                             myArray.push(this.innerHTML);
                             console.log(myArray);
                         }
                         else{
-                            sCreen.value += this.innerHTML;
-                            myArray.push(this.innerHTML);
-                            console.log(myArray);
+                            if(expressionState == 1){
+                                const expression = myArray.join("");
+                                toTal = eval(expression);
+                                myArray = [];
+                                myArray.push(toTal);
+                                myArray.push(this.innerHTML);
+                                exPression.innerHTML = toTal;
+                                exPression.innerHTML += this.innerHTML;
+                                siGn.innerHTML = this.innerHTML;
+                                expressionState = 0;
+                                console.log(myArray);
+                            }
+                            else{
+                                exPression.innerHTML += this.innerHTML;
+                                myArray.push(this.innerHTML);
+                                siGn.innerHTML = this.innerHTML;
+                                console.log(myArray);
+                            }
                         }
                     }
-                    else if (this.innerHTML == '<i class="bi bi-arrow-clockwise"></i>'){
-                        sCreen.value ="";
+                    else if (this.innerHTML == '<i class="bi bi-arrow-counterclockwise"></i>'){
+                        exPression.innerHTML ="";
+                        reSult.innerHTML = "";
+                        siGn.innerHTML = "";
                         myArray = [];
                         console.log(myArray);
                     }
                     else if (this.innerHTML == '<i class="bi bi-arrow-left-circle"></i>'){
-                        myArray.pop();
-                        sCreen.value = myArray.join("");
-                        console.log(myArray);
+                        if(myArray.length == 1){
+                            reSult.innerHTML = "";
+                            siGn.innerHTML = "";
+                            myArray.pop();
+                            exPression.innerHTML = myArray.join("");
+                            console.log(myArray);
+                        }
+                        else{
+                            myArray.pop();
+                            exPression.innerHTML = myArray.join("");
+                            console.log(myArray);
+                        }
                     }
                     else if (this.innerHTML == '<i class="bi bi-info-circle"></i>'){
                         screenState = 1;
-                        sCreen.value ="Developed by Jeffrey Lonzanida using Vanilla Js, HTML5, and CCS3";
+                        expressionState = 1;
+                        exPression.innerHTML ="Developed by Jeffrey Lonzanida";
+                        reSult.innerHTML = "v2.0";
+                        siGn.innerHTML = "";
                         myArray = [];
                         console.log(myArray);
                     }
                     else{
                         if(screenState == 1){
                             screenState = 0;
-                            sCreen.value = this.innerHTML;
+                            reSult.innerHTML = "";
+                            siGn.innerHTML="";
+                            exPression.innerHTML = this.innerHTML;
                             myArray.push(parseFloat(this.innerHTML));
                             console.log(myArray);
                         }
                         else{
-                            sCreen.value += this.innerHTML;
+                            exPression.innerHTML += this.innerHTML;
                             myArray.push(parseFloat(this.innerHTML));
                             console.log(myArray);
                         }
@@ -64,7 +108,10 @@ for (button of buttons){
                 }
                 catch(error){
                     screenState = 1;
-                    sCreen.value = error;
+                    expressionState = 0;
+                    exPression.innerHTML = "Syntax Error";
+                    reSult.innerHTML = "";
+                    siGn.innerHTML = `<i class="bi bi-exclamation-circle-fill"></i>`;
                     myArray = [];
                 }
             }
